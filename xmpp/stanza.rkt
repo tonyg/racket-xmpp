544f91574d4b136521337f90398a21d24f67d8d6
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2016-01-21 19:37:45 -0500

    Stanza logging

--- xmpp/stanza.rkt
+++ xmpp/stanza.rkt
@@ -11,19 +11,24 @@
 
 (require "error.rkt")
 
+(define-logger xmpp)
+
 (define (read-stream-header p)
-  (and (skip-whitespace p)
-       (check-drop p #"<?")
-       (capture-until p 63) ;; #\?
-       (check-drop p #">")
-       (skip-whitespace p)
-       (check-drop p #"<stream:stream")
-       (let ((stream-header-contents (capture-until p 62))) ;; #\>
-         (and stream-header-contents
-              (xml->xexpr (read-xml/element
-                           (open-input-bytes (bytes-append #"<stream:stream"
-                                                           stream-header-contents
-                                                           #"></stream:stream>"))))))))
+  (define header
+    (and (skip-whitespace p)
+         (check-drop p #"<?")
+         (capture-until p 63) ;; #\?
+         (check-drop p #">")
+         (skip-whitespace p)
+         (check-drop p #"<stream:stream")
+         (let ((stream-header-contents (capture-until p 62))) ;; #\>
+           (and stream-header-contents
+                (xml->xexpr (read-xml/element
+                             (open-input-bytes (bytes-append #"<stream:stream"
+                                                             stream-header-contents
+                                                             #"></stream:stream>"))))))))
+  (log-xmpp-debug "Stream header: ~v" header)
+  header)
 
 (define (skip-whitespace p)
   (match (peek-byte p)
@@ -46,16 +51,20 @@
       [b (loop (cons b acc-rev))])))
 
 (define (read-stanza p)
-  (if (eof-object? (peek-byte p))
-      eof
-      (let ((stanza (xml->xexpr (document-element (read-xml/document p)))))
-        (match stanza
-          [`(stream:stream ,_ ,elt) elt] ;; TODO: proper xmlns check
-          [`(stream:stream ,_ ...) ;; TODO: proper xmlns check
-           (xmpp-error "Invalid restarted stream" #:stanza stanza)]
-          [_ stanza]))))
+  (define stanza
+    (if (eof-object? (peek-byte p))
+        eof
+        (let ((stanza (xml->xexpr (document-element (read-xml/document p)))))
+          (match stanza
+            [`(stream:stream ,_ ,elt) elt] ;; TODO: proper xmlns check
+            [`(stream:stream ,_ ...) ;; TODO: proper xmlns check
+             (xmpp-error "Invalid restarted stream" #:stanza stanza)]
+            [_ stanza]))))
+  (log-xmpp-debug "Incoming stanza: ~v" stanza)
+  stanza)
 
 (define (write-stream-header p host)
+  (log-xmpp-debug "Writing stream header for host ~v" host)
   (display "<?xml version=\"1.0\"?>" p)
   (match-define (list _ open-tag)
     (regexp-match #px#"(.*)</stream:stream>"
@@ -68,6 +77,7 @@
   (flush-output p))
 
 (define (write-stanza p stanza #:flush? [flush? #t])
+  (log-xmpp-debug "Outgoing stanza: ~v" stanza)
   (parameterize ((empty-tag-shorthand 'always))
     (write-xexpr stanza p))
   (when flush? (flush-output p)))
